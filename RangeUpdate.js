9d02bf915f6a766c52b2ef139c023deeb9008c2b
InExSu <36605404+InExSu@users.noreply.github.com>
2021-08-11 10:20:57 +0300

    Update RangeUpdate.js

--- RangeUpdate.js
+++ RangeUpdate.js
@@ -1,8 +1,8 @@
 function Range_Update_by_Heads_RUN_Test() {
 
-  Pivot_Duplicate();
+  // Pivot_Duplicate();
 
-  SpreadsheetApp.getActive().getSheetByName('ЗГ Обновление (копия)').activate();
+  SpreadsheetApp.getActive().getSheetByName('ЗГ Обновление').activate();
 
   Range_Update_by_Heads_RUN();
 };
@@ -85,7 +85,7 @@ function Range_Update_by_Heads_RUN() {
 
       // Пусть пользователь проверит соответствия номеров столбцов.
       var choice = ''
-      choice = Browser.msgBox('Найдены столбцы в обоих диапазонах: ' + string_columns, Browser.Buttons.YES_NO);
+      // choice = Browser.msgBox('Найдены столбцы в обоих диапазонах: ' + string_columns, Browser.Buttons.YES_NO);
       if (choice == 'no') {
 
         // Выход по выбору пользователя
@@ -127,14 +127,21 @@ function Range_Update_by_Heads_RUN() {
 }
 
 
-function Range_Update_by_Heads(rng_Old, column_Key_Old, rng_New_In, column_Key_New, a2d_columns, log_make) {
+function Range_Update_by_Heads(rng_Old, column_Key_Old, rng_New, column_Key_New, a2d_columns, log_make) {
 
   // Обновить диапазон по совпадению в ключевых столбцах с учётом наименований столбцов
   // диапазоны в массивы
 
-  var a2d_Old = rng_Old.getValues();
+  // 2021-09-10
+  // var a2d_Old = rng_Old.getValues();
+  // var a2d_New = rng_New.getValues();
+  var a2d_Old = range_2_ArrayValuesFormulas(rng_Old);
+  var a2d_New = range_2_ArrayValuesFormulas(rng_New);
 
-  var a2d_New = rng_New_In.getValues();
+  // в листе "сводная таблица" в столбце Код нулей лидирующих нет,
+  // поэтому удаляю нули из нового. Нет не буду удалять, сделаю нули в ячейках.
+  //  array2dColumnSymbolsLeading(a2d_New, column_Key_New, '0');
+
   var map_Sea = Array2D_2_Map(a2d_New, column_Key_New);
 
   // основное действие
@@ -143,5 +150,161 @@ function Range_Update_by_Heads(rng_Old, column_Key_Old, rng_New_In, column_Key_N
     column_Key_Old, map_Sea, a2d_columns, 'Log');
 
   // массив положить на лист
-  Array2d_2_Range(rng_Old, a2d_Ret);
+  array2d2Range(rng_Old, a2d_Ret);
+}
+
+function rangeZerosAddFrontByFormat_Test() {
+  var sheet = SpreadsheetApp.getActive().getSheetByName('Ошибки');
+  //   var sheetName = sheet.getName();
+
+  //   var cell = sheet.getRange(1, 1);
+  //   cell.setValue("'1"); 
+  //   var cellFormat_old = cell.getNumberFormat();
+  //   cell.setNumberFormat('00000000000');
+  // // чтобы формат обновился стопудово, его надо считать
+  //   var cellFormat_new = cell.getNumberFormat();// НО - апсостроф исчезнет
+
+  //   cell = sheet.getRange(2, 1);
+  //   var cellFormat_old = cell.getNumberFormat();
+  //   cell.setValue("'02");
+  //   // cell.setNumberFormat('00000000000');
+  //   var cellFormat_new = cell.getNumberFormat();
+
+  var rng = sheet.getRange('A1:A3');
+  rangeZerosAddFrontByFormat(rng, '00000000000');
+  rng.setNumberFormat('@');
+  rng.getNumberFormat(); // чтобы проявился setNumberFormats
+}
+
+function selectionNullFormatted() {
+  var choice = Browser.msgBox('В выделенном диапазоне, \n ячейкам с форматом 00000000000 (11 нулей) \n добавит слева апостроф и недостающие нули.\n НЕ быстро.', Browser.Buttons.YES_NO);
+  if (choice === 'yes') {
+    var rng = SpreadsheetApp.getActiveSpreadsheet().getActiveRange();
+    rangeZerosAddFrontByFormat(rng, '00000000000');
+  }
+}
+
+function rangeZerosAddFrontByFormat(rng, formatPattern) {
+
+  const start = new Date().getTime();
+
+  // диапазону добавить лидирующие нули по формату
+  // пусть пользователь сначала присвоит диапазону формат
+
+  var sheet = sheetById(rng.getGridId());
+  var rowStart = rng.getRow();
+  var rowStop_ = rowStart + rng.getNumRows() - 1;
+  var colStart = rng.getRow();
+  var colStop_ = colStart + rng.getNumColumns() - 1;
+
+  for (var row = rowStart; row <= rowStop_; row++) {
+    for (var col = colStart; col <= colStop_; col++) {
+
+      var cell = sheet.getRange(row, col);
+      var formatCell = cell.getNumberFormat();
+
+      if (formatCell === formatPattern) {
+
+        var cellValue = String(cell.getValue());
+
+        if (cellValue.length > 0) {
+
+          var lenDiff = formatPattern.length - cellValue.length;
+
+          if (lenDiff > 0) {
+
+            var symb = formatPattern[0];
+
+            var symbRepeat = symb.repeat(lenDiff);
+
+            cellValue = "'" + symbRepeat + cellValue;
+
+            cell.setValue(cellValue);
+          }
+        }
+      }
+    }
+  }
+  const end = new Date().getTime();
+  Logger.log('rangeZerosAddFrontByFormat время работы: ${end - start}ms');
+  Browser.msgBox('Время работы: ' + String((end - start) / 1000) + ', сек')
+}
+
+function range_2_ArrayValuesFormulas_Test() {
+
+  var sheet = SpreadsheetApp.getActive().getSheetByName('Ошибки');
+  sheet.getRange(1, 1).setValue("значение старое");
+  sheet.getRange(1, 2).setFormula("=SUM(B3:B4)");
+
+  var range = sheet.getRange("A1:B1");
+  range.clearContent;
+
+  var a2d = range_2_ArrayValuesFormulas(range);
+
+  range.setValues(a2d);
+
+  if (String(a2d[0][1]).charAt(0) !== '=') {
+    Logger.log("range_2_ArrayValuesFormulas_Test(): String(a2d[0][1]).charAt(0) !== '='");
+  }
+}
+
+function range_2_ArrayValuesFormulas(range) {
+
+  // из диапазона вернуть массив значений и формул
+
+  var a2d_formul = range.getFormulas();
+  var a2d_values = range.getValues();
+  a2d_Values_add_Formulas(a2d_values, a2d_formul);
+
+  return a2d_values;
+}
+
+function a2d_Values_add_Formulas_test() {
+  var a2d_values = [
+    ["Tom", 1],
+    ["Bill", 1],
+  ];
+  var a2d_formul = [
+    ["=SUM(D3:D4)", 2],
+    ["", 2],
+  ];
+
+  a2d_Values_add_Formulas(a2d_values, a2d_formul);
+
+  if (a2d_values[0][0] == 'Tom') {
+    Logger.log("a2d_Values_add_Formulas_test() - a2d_values[0][0] !== 'Tom'");
+  }
+}
+
+function a2d_Values_add_Formulas(a2d_values, a2d_formul) {
+
+  // в массив 2мерный значений вставить формулы
+  // скопировать ячейки начинающиеся с =
+  // массивы должны быть одинакового размера
+  // потом сделать метод который копирует значения, если в позиции есть символ
+
+  for (let row = 0; row < a2d_values.length; row++) {
+    for (let col = 0; col < a2d_values[0].length; col++) {
+
+      if (String(a2d_formul[row][col]).charAt(0) == '=') {
+
+        a2d_values[row][col] = a2d_formul[row][col];
+
+      }
+    }
+  }
+}
+
+function cells_Compare_Test() {
+
+  // сравнить две ячейки
+  var sheet = SpreadsheetApp.getActive().getSheetByName('Log')
+
+  var cell_01 = sheet.getRange('D4');
+  var valu_01 = parseFloat(cell_01.getValue());
+  var cell_02 = sheet.getRange('E4');
+  var valu_02 = parseFloat(cell_02.getValue());
+
+  Logger.log(typeof valu_01);
+  Logger.log(typeof valu_02);
 }
